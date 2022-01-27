1b0053bc194f66e4064a428a55213319733e8cda
LuisFelipe-0401 <89155083+LuisFelipe-0401@users.noreply.github.com>
2022-01-27 17:37:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Site_Heroku</title>
+</head>
+
+<body>
+  <h1>Site Heroku</h1>
+</body>
+
+</html>
